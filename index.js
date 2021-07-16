0f2960989e92573071c9512364d1e91dd98bc426
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-07-16 15:21:43 +0300

    Validated day , month

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ function getAkan(){
     let birthDate = birthDay.substr(0,2);
     let birthMonth = birthDay.substr(2,2);
     let birthYear = birthDay.substr(4,4);
-    alert(birthDate);
-    alert(birthMonth);
-    alert(birthYear);
+    let  x = validateDate(birthDate,birthMonth);
+    if (x==1){
+        calcAkan();
+    }
+    else{
+        alert("Invalid date!");
+    }
 
     
+}
+function validateDate(day,month){
+    let valid = true;
+    if(day <= 0 || day > 31){
+        valid = false;
+    }
+    else if(month <= 0 || month > 12){
+        valid = false;
+    }
+    if(valid == false){
+        return -1;
+    }
+    else{
+        return 1;
+    }
 }
