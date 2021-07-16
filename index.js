5c8c04e2da537d3007ba750bec797ceee559aa94
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-07-16 15:09:45 +0300

    Separated day, month, year from input

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function getAkan(){
     let birthDay = document.getElementById("birthday").value;
-    birthdate = birthDay.replace("-",",");
-    const date = new Date(birthDay);
+    birthDay.toString();
+    let birthDate = birthDay.substr(0,2);
+    let birthMonth = birthDay.substr(2,2);
+    let birthYear = birthDay.substr(4,4);
+    alert(birthDate);
+    alert(birthMonth);
+    alert(birthYear);
+
     
 }
