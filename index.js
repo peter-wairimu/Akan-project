28ac4c4085c4a707094a77910c477f599f485a74
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-07-17 11:28:47 +0300

    Finalised getting akan names

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function getAkan(){
     let birthDay = document.getElementById("birthday").value;
-    let gender = document.querySelector('input[name="gender"]').value;
+    let gender = getGender(document.querySelectorAll('input[name="gender"]'));
     birthDay.toString();
     let birthDate = birthDay.substr(0,2);
     let birthMonth = birthDay.substr(2,2);
     let birthYear = birthDay.substr(4,4);
     let  x = validateDate(birthDate,birthMonth);
     if (x==1){
-        calcAkan(birthDate, birthMonth, birthYear);
-        alert(day);
+        let day = calcAkan(birthDate, birthMonth, birthYear);
+        // alert(day);
         output(gender,day);
     }
     else{
@@ -17,9 +17,9 @@ function getAkan(){
 
     
 }
-const akanMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-alert(akanMale[0]);
-const akanFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+const akanMale = ["Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame","Kwasi"];
+// alert(akanMale[0]);
+const akanFemale = ["Adwoa","Abenaa","Akua","Yaa","Afua","Ama","Akosua"];
 function validateDate(day,month){
     let valid = true;
     if(day <= 0 || day > 31){
@@ -40,12 +40,13 @@ function calcAkan(day, month, year){
     DD = day;
     CC = year.substr(0,2);
     YY = year.substr(2,2);
-    alert(CC)
-    d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) CC ) + ((26*(MM+1)/10)) + DD );
-    alert(d%7);
-    return d;
+    // alert(CC)
+    let d1 = ( ( (CC/4) -2*CC-1) + Math.ceil((5*YY/4) ) + Math.ceil((26*(MM+1)/10)) + DD ) % 7
+    // alert(d1);
+    return d1;
 }
 function output(gender,day){
+    
     if(gender=="male"){
         alert(akanMale[day]);
     }
@@ -53,6 +54,17 @@ function output(gender,day){
         alert(akanFemale[day]);
     }
     else{
-        alert("error");
+        alert("Invalid gender");
+    }
+}
+function getGender(ele){
+    if (ele[0].checked){
+        return "male";
+    }
+    else if(ele[1].checked){
+        return "female";
+    }
+    else{
+        return "";
     }
 }
